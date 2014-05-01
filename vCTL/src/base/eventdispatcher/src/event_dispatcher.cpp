546f7cb6ed1a2869a2e7761787bdecb3a2028826
Virendra Shakya <drvirens@gmail.com>
2014-05-01 01:12:41 -0700

    event dispatcher empty implementation

--- vCTL/src/base/eventdispatcher/src/event_dispatcher.cpp
+++ vCTL/src/base/eventdispatcher/src/event_dispatcher.cpp
@@ -6,4 +6,86 @@
 //  Copyright (c) 2014 Virendra Shakya. All rights reserved.
 //
 
-#include "event_dispatcher.h"
+#include "base/eventdispatcher/event_dispatcher.h"
+
+namespace vbase
+{
+
+MEventDispatcher::~MEventDispatcher()
+    {
+        
+    }
+    
+MEventDispatcher* MEventDispatcher::New()
+    {
+    MEventDispatcher* obj = new MEventDispatcher();
+    if( obj )
+        {
+        obj->Construct();
+        }
+    return obj;
+    }
+    
+bool MEventDispatcher::PerformWork()
+    {
+    return true;
+    }
+    
+bool MEventDispatcher::PerformIdleWork()
+    {
+    return true;
+    }
+    
+void MEventDispatcher::OnTaskAdded(TTask* aTask)
+    {
+        
+    }
+    
+void MEventDispatcher::OnTaskRemoved(TTask* aTask)
+    {
+        
+    }
+    
+void MEventDispatcher::RegisterDestructorSpectator(const IEventDispatcherDestructorSpectator& aSpectator)
+    {
+        
+    }
+    
+void MEventDispatcher::UnRegisterDestructorSpectator(const IEventDispatcherDestructorSpectator& aSpectator)
+    {
+        
+    }
+    
+void MEventDispatcher::RegisterTaskSpectator(const ITaskSpectator& aSpectator)
+    {
+        
+    }
+    
+void MEventDispatcher::UnRegisterTaskSpectator(const ITaskSpectator& aSpectator)
+    {
+        
+    }
+    
+void MEventDispatcher::ExecuteAsynch(const TLambda& aLambda)
+    {
+        
+    }
+    
+void MEventDispatcher::ExecuteAsynchAfterDelay(const TLambda& aLambda, const TTimeDelta& aTimeDelta)
+    {
+        
+        
+    }
+    
+MEventDispatcher::MEventDispatcher()
+    : iPendingTasksQ(*this)
+    {
+        
+    }
+    
+void MEventDispatcher::Construct()
+    {
+        
+    }
+
+} //namespace vbase
